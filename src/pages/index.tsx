504e07fab5f1f332b9772703e25bc4ff13ff1c69
Alex <balkanskiy.alexey@gmail.com>
2025-01-08 17:24:12 +0100

    Add language selector

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,33 @@
-import { useState } from "react";
-import { Input } from "@nextui-org/input";
+import { useEffect, useState } from "react";
+import { Input, Textarea } from "@nextui-org/input";
 import { Button } from "@nextui-org/button";
-import { title } from "@/components/primitives";
-import {Card, CardBody} from "@nextui-org/react";
-import {CircularProgress} from "@nextui-org/progress";
-
+import { Card, CardBody } from "@nextui-org/react";
+import { CircularProgress } from "@nextui-org/progress";
+import { Select, SelectItem } from "@nextui-org/select";
 import OpenAI from "openai";
-import { useEffect } from "react";
+import { title } from "@/components/primitives";
+
+enum Languages {
+  English = "en",
+  Russian = "ru",
+  German = "de",
+}
+export const languages = [
+  { key: Languages.English, label: "English" },
+  { key: Languages.Russian, label: "Russian" },
+  { key: Languages.German, label: "German" },
+];
+
+const getPrompt = (lang: Languages) => {
+  switch (lang) {
+    case Languages.Russian:
+      return "растолкуй эти карты таро";
+    case Languages.German:
+      return "Interpretieren Sie diese Tarotkarten";
+    default:
+      return "Interpret these tarot cards";
+  }
+};
 
 const openai = new OpenAI({
   dangerouslyAllowBrowser: true,
@@ -14,7 +35,7 @@ const openai = new OpenAI({
     "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",
 });
 
-const getVision = async (base64_image: string) => {
+const getVision = async (base64_image: string, prompt) => {
   if (base64_image) {
     const response = await openai.chat.completions.create({
       model: "gpt-4o-mini",
@@ -22,7 +43,7 @@ const getVision = async (base64_image: string) => {
         {
           role: "user",
           content: [
-            { type: "text", text: "растолкуй эти карты таро" },
+            { type: "text", text: `${prompt}` },
             {
               type: "image_url",
               image_url: {
@@ -41,27 +62,34 @@ const getVision = async (base64_image: string) => {
 };
 
 export default function IndexPage() {
-  const [value, setValue] = useState("");
+  const [selectedLanguage, setSelectedLanguage] = useState(Languages.English);
+  const [prompt, setPrompt] = useState<any>(null);
+
   const [isLoading, setIsLoading] = useState(false);
   const [answer, setAnswer] = useState<any>("No answer yet");
   const [base64String, setBase64String] = useState<string | null>(null);
 
-  useEffect(() => {
-    // completion.then((result) => console.log(result.choices[0].message));
-  }, []);
+  const selectLanguage = (e: any) => {
+    setSelectedLanguage(e.target.value);
+  };
 
   const getAnswer = async () => {
     if (base64String) {
       setIsLoading(true);
-      const vision = await getVision(base64String);
+      const vision = await getVision(base64String, prompt);
 
       setAnswer(vision);
       setIsLoading(false);
     }
   };
 
+  useEffect(() => {
+    setPrompt(getPrompt(selectedLanguage));
+  }, [selectedLanguage]);
+
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
+
     if (file) {
       const reader = new FileReader();
 
@@ -79,19 +107,33 @@ export default function IndexPage() {
       {isLoading && <CircularProgress />}
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
-          <span className={title()}>Neural&nbsp;</span>
+          <span className={title({ color: "cyan" })}>Neural&nbsp;</span>
           <span className={title({ color: "violet" })}>T&nbsp;</span>
         </div>
 
+        <Select
+          className="max-w-xs"
+          label="Language"
+          placeholder="Select Language"
+          selectedKeys={[selectedLanguage]}
+          onChange={selectLanguage}
+        >
+          {languages.map((language) => (
+            <SelectItem key={language.key}>{language.label}</SelectItem>
+          ))}
+        </Select>
+
         <div className="mt-8">
-          <Input
+          <Textarea
+            className="max-w-xs"
+            label="Prompt"
             placeholder="Type something"
-            value={value}
-            onValueChange={setValue}
+            value={prompt}
+            onValueChange={setPrompt}
           />
           <Input type={"file"} onChange={handleFileChange} />
-          <p className="text-default-500 text-small">Input value: {value}</p>
-          <Button color="primary" onPress={getAnswer}>
+          <p className="text-default-500 text-small">Input value: {prompt}</p>
+          <Button color="primary" isDisabled={base64String === null || prompt === null} onPress={getAnswer}>
             get vision
           </Button>
           <Card>
@@ -99,11 +141,11 @@ export default function IndexPage() {
               <p>{answer}</p>
             </CardBody>
           </Card>
-          <img src={base64String || ""} alt="" />
+          <img alt="" src={base64String || ""} />
           {base64String && (
             <div>
               <p>Base64 String:</p>
-              <textarea value={base64String} readOnly rows={10} cols={50} />
+              <textarea readOnly cols={50} rows={10} value={base64String} />
             </div>
           )}
         </div>
